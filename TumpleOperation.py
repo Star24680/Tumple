1f32cbe02353bc59dc882cc746f121a02bf8dc83
Star24680 <gulerumaan1@gmail.com>
2024-10-30 17:47:23 +0500

    first commit

---
+++ TumpleOperation.py
@@ -0,0 +1,13 @@
+TupleX=("Tuple",False,3.2,1)
+print(TupleX)
+TupleY=(4,6,8,3,1)
+print(TupleY)
+TupleY=TupleY+(9,)
+print (TupleY)
+TupleZ=(50,10,60,70,50)
+print(TupleZ.count(50))
+TupleA=(2,8,6,8,10,15,20,20,8,25)
+Slice=TupleA[3:5]
+print (Slice)
+Slice1=TupleA[:6]
+print(Slice1)
